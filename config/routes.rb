be2f1d6c4a383a4ed118e3ec79b9fc96f8a04777
celinechege <chegeceline18@gmail.com>
2023-07-03 00:11:35 +0300

    complete assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+  resource :login, only: [:create]
+  resource :logout, only: [:destroy]
+  resource :me, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
